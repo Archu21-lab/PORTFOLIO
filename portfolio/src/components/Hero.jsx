a796476332994e0d5ab060fe4bb369221b2350a3
Archu21-lab <archupatil5958@gmail.com>
2026-06-30 14:02:57 +0530

    ..

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { easeOut, motion } from "framer-motion";
+import { assets } from "../assets/assets";
 
 export default function Hero() {
   return (
@@ -10,6 +11,45 @@ export default function Hero() {
       viewport={{ once: true }}
       id="home"
       className="min-h-screen flex items-center pt-20 pb-16 bg-gradient-to-r from-[#1a1a1a] via-[#2d2d2d] to-[#1a1a1a]"
-    ></motion.div>
+    >
+      <div className="container mx-auto px-6 flex flex-col md:flex-row items-center justify-between">
+        {/* left side content */}
+        <div className="md:w-1/2 mb:10 md:mb-0">
+          <h1 className="text-4xl md:text-6xl font-bold mb-8">
+            Hi, I'm <span className="text-purple">Archana Patil</span>
+          </h1>
+          <h2 className="text-2xl md:text-4xl font-semibold mb-8 typewriter">
+            Full Stack Developer
+          </h2>
+          <p className="text-lg text-gray-300 mb-12">
+            I create stunning web experiences with modern technologies and
+            innovative design
+          </p>
+          <div className="flex gap-6">
+            <a
+              href="#projects"
+              className="w-36 h-12 px-6 flex items-center justify-center py-3 bg-purple rounded-lg font-medium hover:bg-purple-700 transition duration-300"
+            >
+              View Work
+            </a>
+            <a
+              href="#contact"
+              className="w-36 p-3 h-12 px-6 py-3 flex items-center justify-center border border-purple rounded-lg font-medium hover:bg-purple/20 transition decoration-purple-300"
+            >
+              Contact Me
+            </a>
+          </div>
+         
+        </div>
+        {/* right side content  */}
+        <div className="md:w-1/2 flex justify-center">
+          <div className="relative w-64 h-64 md:w-80 md:h-80">
+            <div className="absolute inset-0 rounded-full bg-gradient-to-r from-purple to-pink animate-pluse-slow-opacity-70">
+              <motion.img className="relative rounded-full w-64 h-64 md:w-80 md:h-80 object-cover z-10 animate-float" src={assets.MyImg1} alt="profile"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.div>
   );
 }
